8395c7568aa83689be989e9e6fe31d2bac03c3a4
Ahmet Yasir <ahmetyasirkulaksiz@gmail.com>
2024-02-05 05:03:00 -0500

    ex08

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akulaksi <akulaksi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 04:43:32 by akulaksi          #+#    #+#             */
+/*   Updated: 2024/02/05 05:02:30 by akulaksi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void    ft_swap (int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void    ft_sort_int_tab(int *tab, int size)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (i < size)
+    {
+        j = 0;
+        while (j < size)
+        {
+            if (tab[j] > tab[i])
+                ft_swap(&tab[i], &tab[j]);
+            j++;
+        }
+        i++;
+    }
+}
+
+/*int main (void)
+{
+    int arr[]= {4, 3, 7, -6, 5, 4, 3, 1, 0, 0};
+    int i;
+    
+    i = 0;
+    while (i < 9)
+    {
+        printf("%d ", arr[i]);
+        i++;      
+    }
+    printf("\n");
+    ft_sort_int_tab(arr, 9);
+    i = 0;
+    while (i < 9)
+    {
+        printf("%d ", arr[i]);
+        i++;      
+    }
+}*/
