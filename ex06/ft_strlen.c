dcaf1ccac436089dc4a5d94288ce6427042b0553
Ahmet Yasir <ahmetyasirkulaksiz@gmail.com>
2024-02-05 04:18:35 -0500

    ex06

---
+++ ex06/ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akulaksi <akulaksi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 04:13:16 by akulaksi          #+#    #+#             */
+/*   Updated: 2024/02/05 04:18:15 by akulaksi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+/*int main (void)
+{
+    char *str = "STDOUT";
+    printf("%d",ft_strlen(str));
+    return (0);
+}*/
