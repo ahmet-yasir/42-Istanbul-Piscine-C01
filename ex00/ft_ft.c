11edf58ae9f48751c5f8459ff5ed2fe977923367
Ahmet Yasir <ahmetyasirkulaksiz@gmail.com>
2024-02-05 03:15:47 -0500

    ex00

---
+++ ex00/ft_ft.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akulaksi <akulaksi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 03:01:16 by akulaksi          #+#    #+#             */
+/*   Updated: 2024/02/05 03:14:45 by akulaksi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void    ft_ft (int *nbr)
+{
+	*nbr = 42;
+}
+
+/*int	main (void)
+{
+	int *num;
+
+	num = (int *)malloc(sizeof(int));
+	ft_ft(num);
+	printf("%d",*num);
+	return (0);
+}*/
